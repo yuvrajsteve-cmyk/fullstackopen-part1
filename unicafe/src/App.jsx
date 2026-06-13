c72dd5ec2a27d76d17f5278ef71d3f232a85559a
Yuvraj Singh <yuvrajsteve@email.com>
2026-06-13 12:46:10 +0530

    unicafe exercise 1.7

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -7,6 +7,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const total = good + neutral + bad
+  const average = total === 0 ? 0 : (good - bad) / total
+  const positive = total === 0 ? 0 : (good / total) * 100
+
   return (
     <div>
       <h1>Give me feedback</h1>
@@ -19,6 +23,11 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+
+      
     </div>
   )
 }
